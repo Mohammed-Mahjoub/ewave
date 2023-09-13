30fbf8f7c22cb35d54c2a7092e4d72fd91dbb341
Mohammed-shaban <mohshaban712@gmail.com>
2023-09-13 12:07:40 +0300

    ios

--- lib/models/Noti.dart
+++ lib/models/Noti.dart
@@ -3,8 +3,9 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 class Noti{
   static Future initialize(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,) async {
     var androidInitialize = new AndroidInitializationSettings('mipmap/ic_launcher');
-
+    final DarwinInitializationSettings initializationSettingsDarwin =  DarwinInitializationSettings();
     var initializationsSettings = new InitializationSettings(android: androidInitialize,
+      iOS: initializationSettingsDarwin
     );
     await flutterLocalNotificationsPlugin.initialize(initializationsSettings);
 
